f32416247677fdf3638846d94d3ecfd41b44b8ff
TomasHaydu <103974880+TomasHaydu@users.noreply.github.com>
2023-01-02 22:03:35 -0300

    Correcion de errores y añadió la ventana modal pro

--- src/components/MoreInfoModal.jsx
+++ src/components/MoreInfoModal.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import usePokemon from "../hooks/UsePokemon";
 import NoFav from "../icons/fav-icon-nofav.png";
 import Fav from "../icons/fav-icon-fav.png";
+import ProgressBar from "./ProgressBar";
 
-const MoreInfoModal = ({pokemon, pokeInfo, modalInfo, setModalInfo, pokemonToFav}) => {
+const MoreInfoModal = ({
+  pokemon,
+  pokeInfo,
+  modalInfo,
+  setModalInfo,
+  pokemonToFav,
+}) => {
 
-    const {colorTypes, fav}  = usePokemon()
+  const { colorTypes, fav } = usePokemon();
+
+
+  const totalCalculator = () => { 
+    let total = 0
+    pokeInfo.stats.map( s => total = (total + (s.base_stat)))
+    return total
+  }
 
   return (
     <div className="container__pokemons_container">
@@ -37,46 +51,69 @@ const MoreInfoModal = ({pokemon, pokeInfo, modalInfo, setModalInfo, pokemonToFav
           </div>
         </div>
         <div className="modal-info">
-          <div className="modal__container">
-            <label className="modal__container--label">NRO: </label>
-            <p className="modal__container--info">{pokeInfo.id}</p>
+          <div className="modal-info-data">
+            <div className="modal__container">
+              <label className="modal__container--label">NRO: </label>
+              <p className="modal__container--info">{pokeInfo.id}</p>
+            </div>
+            <div className="modal__container">
+              <label className="modal__container--label">ALTURA: </label>
+              <p className="modal__container--info">{pokeInfo.height/10}mts</p>
+            </div>
+            <div className="modal__container">
+              <label className="modal__container--label">PESO: </label>
+              <p className="modal__container--info">{pokeInfo.weight/10}kg</p>
+            </div>
+            <div className="modal__container">
+              <label className="modal__container--label">HABILIDADES: </label>
+              {pokeInfo.abilities.map((poke) => (
+                <p className="modal__container--info" key={poke.ability.name}>
+                  {poke.ability.name}
+                </p>
+              ))}
+            </div>
+            <div className="modal__container">
+              <label className="modal__container--label">TIPO: </label>
+              {pokeInfo.types.map((poke) => (
+                <p
+                  className="modal__container--info"
+                  key={poke.type.name}
+                  style={{ backgroundColor: `${colorTypes[poke.type.name]}` }}
+                >
+                  {poke.type.name}
+                </p>
+              ))}
+            </div>
+            <div className="modal__container">
+              <label className="modal__container--label">FORMAS: </label>
+              {pokeInfo.forms.map((poke) => (
+                <p className="modal__container--info" key={poke.name}>
+                  {poke.name}
+                </p>
+              ))}
+            </div>
           </div>
-          <div className="modal__container">
-            <label className="modal__container--label">ALTURA: </label>
-            <p className="modal__container--info">{pokeInfo.height}mts</p>
-          </div>
-          <div className="modal__container">
-            <label className="modal__container--label">PESO: </label>
-            <p className="modal__container--info">{pokeInfo.weight}kg</p>
-          </div>
-          <div className="modal__container">
-            <label className="modal__container--label">HABILIDADES: </label>
-            {pokeInfo.abilities.map((poke) => (
-              <p className="modal__container--info" key={poke.ability.name}>
-                {poke.ability.name}
-              </p>
+          <div className="modal__progress-bar">
+            {pokeInfo.stats.map( s => (
+                           <div className="modal__container"
+                           key={`${s.stat.name}${s.base_stat}` }
+                           >
+                           <label className="modal__container--label">{s.stat.name}</label>
+                           <ProgressBar 
+                           completed={s.base_stat}
+                           total={200}
+                           />
+                         </div>
             ))}
           </div>
-          <div className="modal__container">
-            <label className="modal__container--label">TIPO: </label>
-            {pokeInfo.types.map((poke) => (
+          <div className="modal__total">
+              <label
+              className="modal__total-title"
+              >Total:</label>
               <p
-                className="modal__container--info"
-                key={poke.type.name}
-                style={{ backgroundColor: `${colorTypes[poke.type.name]}` }}
-              >
-                {poke.type.name}
-              </p>
-            ))}
-          </div>
-          <div className="modal__container">
-            <label className="modal__container--label">FORMAS: </label>
-            {pokeInfo.forms.map((poke) => (
-              <p className="modal__container--info" key={poke.name}>
-                {poke.name}
-              </p>
-            ))}
-          </div>
+              className="modal__total-number"
+              >{totalCalculator()}</p>
+            </div>
         </div>
       </div>
       <div>
